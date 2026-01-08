88f747c6be3220bdf1b83a097066e02dbb0ec14d
Washingtonluigy <washingtonluigy2@gmail.com>
2026-01-08 15:26:07 -0300

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ahpdojlajtfejmtjtwvr.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFocGRvamxhanRmZWptdGp0d3ZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4OTQ2MjgsImV4cCI6MjA4MzQ3MDYyOH0.3EHVvsPFnDLm3AZVAm8AdpLIaW9IxLJ5at6VQNT82qk';
 
 const getStorage = () => {
   if (typeof window !== 'undefined' && window.localStorage) {
